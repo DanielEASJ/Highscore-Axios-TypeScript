0886ba9ccfcd0c310022c4b297467cc662ef9207
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-10-22 12:37:55 +0200

    added base

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,23 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios from 'axios';
+
+let URL: string = "http://localhost:3000/";
+
+
+interface Score {
+    name: string;
+    score: number;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+axios.get(URL + 'Api/Score/')
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function getScore(score: Score): string {
+    return score.name + " " + score.score;
+}
+
+
+
+
+// let user: Person = { firstName: "John", lastName: "Doe" };
+
+// let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+// element.innerHTML = greeter(user);
